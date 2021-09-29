988278841052e28901202750533ae2e9a044e8a4
Riey <creeper844@gmail.com>
2021-09-29 20:35:17 +0900

    Print wasm output in example

--- examples/compile.rs
+++ examples/compile.rs
@@ -1,1 +1,8 @@
-fn main() {}
+use clrs_pe::pe::Image;
+
+fn main() {
+    let file = include_bytes!("../assets/HelloWorld.dll");
+    let image = Image::from_bytes(file).unwrap();
+    let wasm = clrs_compiler::compile(&image);
+    println!("{}", wasmprinter::print_bytes(wasm).unwrap());
+}
