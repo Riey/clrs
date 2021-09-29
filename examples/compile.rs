63efa11cbf2d57a690c680e233bae6a16bf687c1
Riey <creeper844@gmail.com>
2021-09-30 02:12:31 +0900

    Example check module valid

--- examples/compile.rs
+++ examples/compile.rs
@@ -4,5 +4,6 @@ fn main() {
     let file = include_bytes!("../assets/HelloWorld.dll");
     let image = Image::from_bytes(file).unwrap();
     let wasm = clrs_compiler::compile(&image);
-    println!("{}", wasmprinter::print_bytes(wasm).unwrap());
+    println!("{}", wasmprinter::print_bytes(&wasm).unwrap());
+    wasmparser::validate(&wasm).unwrap();
 }
